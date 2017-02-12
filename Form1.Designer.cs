d59a9463f2a797c63b1bd1cb4dd0a75d1cdc6e68
Denis Granovskiy <xxPRIMExx@mail.ru>
2017-02-12 19:04:10 +0200

    PaintProject

---
+++ Form1.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace MyPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.Save = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btn_Inversion = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(674, 385);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(707, 26);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(56, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Элипс";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(707, 50);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Прямоугольник";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(707, 74);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(65, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Прямая";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(707, 98);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(76, 17);
+            this.radioButton4.TabIndex = 4;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Карандаш";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(818, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(707, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Цвет";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(707, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Очистить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(707, 222);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(76, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.TextUpdate += new System.EventHandler(this.comboBox1_TextUpdate);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(707, 329);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 10;
+            this.Save.Text = "Сохранить";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // btn_Inversion
+            // 
+            this.btn_Inversion.Location = new System.Drawing.Point(692, 261);
+            this.btn_Inversion.Name = "btn_Inversion";
+            this.btn_Inversion.Size = new System.Drawing.Size(114, 23);
+            this.btn_Inversion.TabIndex = 11;
+            this.btn_Inversion.Text = "Инвертировать";
+            this.btn_Inversion.UseVisualStyleBackColor = true;
+            this.btn_Inversion.Click += new System.EventHandler(this.btn_Inversion_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(707, 359);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Загрузить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(692, 291);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(114, 23);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(708, 191);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Закрыть";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.saveAsToolStripMenuItem.Text = "Save as...";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(818, 423);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.btn_Inversion);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btn_Inversion;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MyPaint
+{
+
+    enum UserTool
+    {
+        None,
+        Line,
+        Rectangle,
+        Circle,
+        Pen
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool press = false;             // Флаговая переменная нажатия ЛКМ
+
+        int x1;                         // Координаты для рисования фигур
+        int y1;                         // Координаты для рисования фигур
+
+        int _width;                     // Ширина фигуры
+        int _height;                    // Высота фигуры
+
+        Point MousePressed;             // Точка на плоскости, в которой была нажата ЛКМ
+        Point MouseUnpressed;           // Точка на плоскости, в которой была нажата ПКМ
+
+        Color Current = Color.Black;    // Текущий цвет пользователя
+        int thickness = 1;              // Толщина линии
+
+        public Bitmap bmp = new Bitmap(674, 385);  // Создаем картинку в которой будем выполнять рисование
+       
+        /// <summary>
+        /// Решил сделать выбор пользователя перечислениями, на мой взгляд, код становиться более читебелен,
+        /// Нежели указывать номера кнопок (решил кнопки не переименовывать).
+        /// </summary>
+        private UserTool UserChiose()
+        {
+            UserTool UI = UserTool.None;
+            if (radioButton1.Checked)
+            {
+                UI = UserTool.Circle;
+            }
+            if (radioButton2.Checked)
+            {
+                UI = UserTool.Rectangle;
+            }
+            if (radioButton3.Checked)
+            {
+                UI = UserTool.Line;
+            }
+            if (radioButton4.Checked)
+            {
+                UI = UserTool.Pen;
+            }
+            return UI;
+        }
+
+        /// <summary>
+        /// Меню диалога выбора цвета для пользовтеля
+        /// </summary>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult D = colorDialog1.ShowDialog();         // Вывод диалога о выборе цвета
+            if (D == DialogResult.OK)                           // Если диалг выведен
+            {
+                Current = colorDialog1.Color;                   // Применяем выбранный цвет
+            }
+        }
+
+        /// <summary>
+        /// Выбор толщины линии
+        /// </summary>
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            thickness = int.Parse(comboBox1.Text);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Image = bmp;
+            DoubleBuffered = true;
+            progressBar1.Visible = false;
+
+        }
+
+        /// <summary>
+        /// Функция "кисть", отслеживается текущее положение мыши, текущее положение является началом, и концом для 
+        /// отрисовки линии. т.е. получаем точку
+        /// </summary>
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics g = Graphics.FromImage(pictureBox1.Image);                     
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;  // Сглаживание "фигур"
+
+            if (press)                                  // Если зажата мышь, начальная координата будет перемещаться
+            {
+                MouseUnpressed = e.Location;            // моменты "отжатия" являются текущим положением мыши
+
+                if (UserChiose() == UserTool.Pen)       // Рисование кистью
+                {
+                    g.DrawLine(new Pen(Current, thickness), MousePressed, MouseUnpressed);  // Рисуем  "точку"
+
+                    MousePressed = MouseUnpressed;      // "Предыдущая" координата для отрисовки "линии", которая превратится в точку
+                }
+            }
+            pictureBox1.Refresh();                      // Обновляем картинку
+        }
+
+        /// <summary>
+        /// Рисуем фигуры на "поле"
+        /// </summary>
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Pen MyPen = new Pen(Current, thickness);    // Выбранный тип кисти
+
+            if (press)                                  // Условие для рисования
+            {
+                x1 = Math.Min(MousePressed.X, MouseUnpressed.X);
+                y1 = Math.Min(MousePressed.Y, MouseUnpressed.Y);
+
+                _width = Math.Abs(MousePressed.X - MouseUnpressed.X);
+                _height = Math.Abs(MousePressed.Y - MouseUnpressed.Y);
+
+                if (UserChiose() == UserTool.Rectangle)
+                {
+                    e.Graphics.DrawRectangle(MyPen, x1, y1, _width, _height);
+                }
+                if (UserChiose() == UserTool.Circle)
+                {
+                    e.Graphics.DrawEllipse(MyPen, x1, y1, _width, _height); 
+                }
+                if (UserChiose() == UserTool.Line)
+                {
+                    e.Graphics.DrawLine(MyPen, MousePressed, MouseUnpressed);
+                }
+            }
+        }
+
+        /// <summary>
+        /// При отпускании ЛКМ, сохраняем фигуры на поле.
+        /// </summary>
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            Pen MyPen = new Pen(Current, thickness);
+
+            press = false;
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+
+            if (UserChiose() == UserTool.Circle)
+            {
+                g.DrawEllipse(MyPen, x1, y1, _width, _height);
+            }
+            if (UserChiose() == UserTool.Rectangle)
+            {
+                g.DrawRectangle(MyPen, x1, y1, _width, _height);
+            }
+            if (UserChiose() == UserTool.Line)
+            {
+                g.DrawLine(MyPen, MousePressed, MouseUnpressed);
+            }
+            g.Save();       // Сохраняем изменения
+        }
+
+        /// <summary>
+        /// Флаг нажатия клавиши, сохраняем координаты нажатия мыши
+        /// </summary>
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            press = true;
+            MousePressed = e.Location;
+        }
+
+        /// <summary>
+        /// Очищаем рисунок, накладываем пустой рисунок поверх имеющегося
+        /// </summary>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bit2 = new Bitmap(674, 385);
+            pictureBox1.Image = bit2;
+
+            pictureBox1.Invalidate();       // Обновляем Picturebox
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// Толщина шрифта, при неверном вводе, сохраняется старый параметр
+        /// </summary>
+        private void comboBox1_TextUpdate(object sender, EventArgs e)
+        {
+            int.TryParse(comboBox1.Text, out thickness);
+        }
+
+        /// <summary>
+        /// Меню сохранение картинки
+        /// </summary>
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Bitmap bmpSave = bmp;
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.DefaultExt = "bmp";
+            sfd.Filter = "Image Files (*.bmp) | *.bmp |All files (*.*)|*.*";
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                bmpSave.Save(sfd.FileName);
+            }
+
+        }
+
+        /// <summary>
+        /// Выпадающее меню
+        /// </summary>
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        /// <summary>
+        /// Открытие BMP файла 
+        /// </summary>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.Filter = "All Files|*.*";
+
+            openFile.DefaultExt = "bmp";
+
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image = Image.FromFile(openFile.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Инверсия рисунка, выполняем в отдельном потоке с помощью Background Worker'a
+        /// </summary>
+        private void btn_Inversion_Click(object sender, EventArgs e)
+        {
+            progressBar1.Visible = true;
+            btn_Inversion.Enabled = false;          // Деактивируем кнопку
+            pictureBox1.Enabled = false;
+
+            backgroundWorker1.RunWorkerAsync();     // Запускаем задачу в фоне
+        }
+
+        
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            IsInverse();        // Задача для фоновой обработки
+        }
+
+        /// <summary>
+        /// Метод, инвертирующий цвета на рисунке, 
+        /// </summary>
+        private void IsInverse()
+        {
+            int status = bmp.Width;     // Считаем внешний цикл как "индикатор"
+            int currentStatus = 0;      // Текущее состояние обработки
+
+            for (int x = 0; x <= bmp.Width - 1; x++, currentStatus++)
+            {
+                backgroundWorker1.ReportProgress((x * 100) / status);   // Отчет % выполнения для backgroundWorker'a
+
+                for (int y = 0; y <= bmp.Height - 1; y++)
+                {
+                    Color oldColor = bmp.GetPixel(x, y);
+                    Color newColor = Color.FromArgb(oldColor.A, 255 - oldColor.R, 255 - oldColor.G, 255 - oldColor.B);  // Инверсия RGB цветов
+                    bmp.SetPixel(x, y, newColor);
+                }
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage + 1;  // такой-себе костыль, для корректной отработки прогресс-бара
+
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+
+            btn_Inversion.Enabled = true;
+            pictureBox1.Enabled = true;
+            progressBar1.Visible = false;
+
+            pictureBox1.Refresh();                          // Обновляем картинку по завершении задачи
+        }
+
+    }
+}
